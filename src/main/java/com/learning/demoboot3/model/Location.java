0185369976ce0229844c0672e6f9bcdaebdf80ef
er.vinit@gmail.com <er.vinit@gmail.com>
2023-04-02 23:10:44 -0400

    replace pojo to record

--- src/main/java/com/learning/demoboot3/model/Location.java
+++ src/main/java/com/learning/demoboot3/model/Location.java
@@ -1,12 +1,4 @@
 package com.learning.demoboot3.model;
 
-import lombok.Data;
-
-@Data
-public class Location {
-    private Integer id;
-    private String street;
-    private String city;
-    private String state;
-    private String pin;
+public record Location(Integer id, String street, String city, String state, String pin) {
 }
